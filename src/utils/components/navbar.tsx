4e968d23ddad810e544776ddf4b4cb717b6759be
intern-huiting <itshuitingg@gmail.com>
2021-08-09 20:19:44 +0800

    Fix Hash History cannot push same path

--- src/utils/components/navbar.tsx
+++ src/utils/components/navbar.tsx
@@ -11,23 +11,23 @@ class NavBar extends React.Component {
     return (
       <div id="navbar" className={components.navbar}>
         <div id="first_page_block" className={app.first_page_block} />
-        <Link to="/">
+        <Link to="/" replace>
           <img src={totoro} alt="homepage button" />
           {window.location.hash && (
             <span className={components.homelink}>huiting lee</span>
           )}
         </Link>
         <div className={components.links}>
-          <Link className={components.link} to="/about">
+          <Link className={components.link} to="/about" replace>
             About
           </Link>
-          <Link className={components.link} to="/experience">
+          <Link className={components.link} to="/experience" replace>
             Experience
           </Link>
-          <Link className={components.link} to="/projects">
+          <Link className={components.link} to="/projects" replace>
             Projects
           </Link>
-          <Link className={components.link} to="/contact">
+          <Link className={components.link} to="/contact" replace>
             Contact
           </Link>
         </div>
